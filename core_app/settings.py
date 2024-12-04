b1d3e1c8740c0eba8b7a1b5dc9d330ff5ff52507
shaikmoahmmedisaq <122245118+shaikmoahmmedisaq@users.noreply.github.com>
2024-12-04 10:44:08 +0530

    Update settings.py

--- core_app/settings.py
+++ core_app/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
         
 ]
 
@@ -139,7 +140,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
-
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
